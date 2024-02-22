69a7c585c1b75e3cb7c0f6fe8b894548ddb66886
Crowdbotics <team@crowdbotics.com>
2024-02-22 02:16:24 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "red-pine-46556",
+  url: "https://red-pine-46556.botics.co",
+  api: "https://red-pine-46556.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
